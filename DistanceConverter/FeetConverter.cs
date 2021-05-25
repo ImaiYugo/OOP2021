6c3ff2a352b8aef38dc4a3fdbe9ea0652087ad90
ImaiYugo <32228@ojs.ac.jp>
2021-05-25 10:38:11 +0900

    p54リスト2.9 静的クラスへの変更

---
+++ DistanceConverter/FeetConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public static class FeetConverter {
+
+        public static double FromMeter(double meter) {
+            return meter / 0.3048;
+        }
+        public static double ToMeter(double feet) {
+            return feet * 0.3048;
+        }    
+    }
+}
--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -14,22 +14,17 @@ namespace DistanceConverter {
         }
         static void PrintFeetToMeterList(int start, int stop) {
             for (int feet = start; feet <= stop; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0}ft= {1:0.0000}m", feet, meter);
             }
         }
 
         static void PrintMeterToFeetList(int start, int stop) {
             for (int meter = start; meter <= stop; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0}m= {1:0.0000}ft", meter, feet);
             }
         }
-        private static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-        private static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
+
     }
 }
