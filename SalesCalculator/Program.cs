72d4235c1e03588aca5d6b85dce6dc896e9e9c85
ImaiYugo <32228@ojs.ac.jp>
2021-05-28 14:05:21 +0900

    p77リスト2-26

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter("Sales.csv");
+            var sales = new SalesCounter("Sales.csv");
             //List<Sale> sales = ReadSales("Sales.csv");
 
-           Dictionary<string,int> amountPerStrore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string,int> obj in amountPerStrore) {
+           var amountPerStrore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStrore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
         }
--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -8,16 +8,16 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     //売上集計クラス
     class SalesCounter {
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         //コンストラクタ
         public SalesCounter(string filePath) {
             _sales = ReadSales(filePath);
         }
-        private static List<Sale> ReadSales(string filePath) {
+        private static IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) {
+            foreach (var line in lines) {
                 string[] items = line.Split(',');
                 Sale sale = new Sale {
                     ShopName = items[0],
@@ -28,9 +28,9 @@ namespace SalesCalculator {
             }
             return sales;
         }
-        public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach(Sale sale in _sales) {
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach(var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     //既にコレクションに店舗が設定されている
                     dict[sale.ShopName] += sale.Amount;
