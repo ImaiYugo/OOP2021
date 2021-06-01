7f9518b89d8499f6852eab111e18cbf4181b5961
ImaiYugo <32228@ojs.ac.jp>
2021-06-01 10:25:41 +0900

    exercise2

--- Exercise2/InchConverter.cs
+++ Exercise2/InchConverter.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise2 {
-    //フィートとメートルの単位変換クラス
-    public static class FeetConverter {
-        private const double ratio = 0.3048;
+    //インチとメートルの単位変換クラス（静的クラス）
+    public static class InchConverter {
+        private const double ratio = 0.0254;
 
+        //メートルからインチを求める
         public static double FromMeter(double meter) {
             return meter / ratio;
         }
+        //インチからメートルを求める
         public static double ToMeter(double feet) {
             return feet * ratio;
         }    
