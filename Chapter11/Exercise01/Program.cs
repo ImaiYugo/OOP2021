35f356c70978dd3a8087a15fa556c7b15156b15d
優吾 今井 <32228@ojs.ac.jp>
2021-08-27 16:00:38 +0900

    11-2

--- Chapter11/Exercise01/Program.cs
+++ Chapter11/Exercise01/Program.cs
@@ -24,26 +24,45 @@ namespace Exercise01
         private static void Exercise01_1(string file)
         {
             var xdoc = XDocument.Load(file);
-            foreach (var xnovelist in xdoc.Root.Elements())
-            {
-                var xname = xnovelist.Element("name");
-                var xno = xnovelist.Element("teammembers");
-                Console.WriteLine("{0},{1}", xname.Value, xno.Value);
-            }
+            var sports = xdoc.Root.Elements()
+                                  .Select(x => new
+                                  {
+                                      Name = x.Element("name").Value,
+                                      Teammembers = x.Element("teammembers").Value
+                                  });
+            //foreach (var xnovelist in xdoc.Root.Elements())
+            //{
+            //    var xname = xnovelist.Element("name");
+            //    var xno = xnovelist.Element("teammembers");
+            //    Console.WriteLine("{0},{1}", xname.Value, xno.Value);
+            //}
         }
 
         private static void Exercise01_2(string file)
         {
             var xdoc = XDocument.Load(file);
-            var xnovelists = xdoc.Root.Elements()
-                                 .OrderBy(x => x.Element("firstplayed"));
+            var sports = xdoc.Root.Elements()
+                                  .Select(x => new
+                                  {
+                                      Firstplayed = x.Element("firstplayed").Value,
+                                      Name = x.Element("name").Attribute("kanji").Value
+                                  }).OrderBy(x => int.Parse(x.Firstplayed));
 
-            foreach (var xnovelist in xnovelists)
+            foreach (var sport in sports)
             {
-                var xname = xnovelist.Element("name kanji");
-                Console.WriteLine("{0}",xname);
+                Console.WriteLine("{0} {1}", sport.Name, sport.Firstplayed);
             }
         }
+ 
+        //    var xnovelists = xdoc.Root.Elements()
+        //                         .OrderBy(x => x.Element("firstplayed"));
+
+        //    foreach (var xnovelist in xnovelists)
+        //    {
+        //        var xname = xnovelist.Element("name kanji");
+        //        Console.WriteLine("{0}",xname);
+        //    }
+        
 
         private static void Exercise01_3(string file)
         {
