5164cdb207453a80f167bf2a83d1779bf9a0be3b
優吾 今井 <32228@ojs.ac.jp>
2021-10-22 15:41:35 +0900

    1022

---
+++ ADO.NET/AddressBook/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace AddressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.personDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.infosys202132DataSet = new AddressBook.infosys202132DataSet();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.personTableAdapter = new AddressBook.infosys202132DataSetTableAdapters.PersonTableAdapter();
+            this.tableAdapterManager = new AddressBook.infosys202132DataSetTableAdapters.TableAdapterManager();
+            this.btNameFilter = new System.Windows.Forms.Button();
+            this.tbNameSearch = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.tbTel = new System.Windows.Forms.TextBox();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infosys202132DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // personDataGridView
+            // 
+            this.personDataGridView.AllowUserToAddRows = false;
+            this.personDataGridView.AutoGenerateColumns = false;
+            this.personDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.personDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.personDataGridView.DataSource = this.personBindingSource;
+            this.personDataGridView.Location = new System.Drawing.Point(8, 210);
+            this.personDataGridView.MultiSelect = false;
+            this.personDataGridView.Name = "personDataGridView";
+            this.personDataGridView.RowTemplate.Height = 21;
+            this.personDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.personDataGridView.Size = new System.Drawing.Size(800, 155);
+            this.personDataGridView.TabIndex = 1;
+            this.personDataGridView.SelectionChanged += new System.EventHandler(this.personDataGridView_SelectionChanged);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "No";
+            this.dataGridViewTextBoxColumn1.HeaderText = "No";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Birthday";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Birthday";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Telephone";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Telephone";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Memo";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Memo";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataMember = "Person";
+            this.personBindingSource.DataSource = this.infosys202132DataSet;
+            // 
+            // infosys202132DataSet
+            // 
+            this.infosys202132DataSet.DataSetName = "infosys202132DataSet";
+            this.infosys202132DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(8, 371);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(63, 30);
+            this.btConnect.TabIndex = 2;
+            this.btConnect.Text = "接続";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // personTableAdapter
+            // 
+            this.personTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PersonTableAdapter = this.personTableAdapter;
+            this.tableAdapterManager.UpdateOrder = AddressBook.infosys202132DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // btNameFilter
+            // 
+            this.btNameFilter.Location = new System.Drawing.Point(314, 374);
+            this.btNameFilter.Name = "btNameFilter";
+            this.btNameFilter.Size = new System.Drawing.Size(63, 30);
+            this.btNameFilter.TabIndex = 2;
+            this.btNameFilter.Text = "名前抽出";
+            this.btNameFilter.UseVisualStyleBackColor = true;
+            this.btNameFilter.Click += new System.EventHandler(this.btNameFilter_Click);
+            // 
+            // tbNameSearch
+            // 
+            this.tbNameSearch.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNameSearch.Location = new System.Drawing.Point(177, 374);
+            this.tbNameSearch.Name = "tbNameSearch";
+            this.tbNameSearch.Size = new System.Drawing.Size(115, 28);
+            this.tbNameSearch.TabIndex = 3;
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(80, 371);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(61, 31);
+            this.btSave.TabIndex = 4;
+            this.btSave.Text = "保存";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "名前";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "誕生日";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "電話番号";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(322, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "備考";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(80, 35);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(203, 19);
+            this.tbName.TabIndex = 6;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(83, 87);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 19);
+            this.dtpDate.TabIndex = 7;
+            // 
+            // tbTel
+            // 
+            this.tbTel.Location = new System.Drawing.Point(80, 136);
+            this.tbTel.Name = "tbTel";
+            this.tbTel.Size = new System.Drawing.Size(203, 19);
+            this.tbTel.TabIndex = 6;
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Location = new System.Drawing.Point(324, 62);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.Size = new System.Drawing.Size(203, 93);
+            this.tbMemo.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.tbTel);
+            this.Controls.Add(this.tbMemo);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.tbNameSearch);
+            this.Controls.Add(this.btNameFilter);
+            this.Controls.Add(this.btConnect);
+            this.Controls.Add(this.personDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.personDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infosys202132DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private infosys202132DataSet infosys202132DataSet;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private infosys202132DataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private infosys202132DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView personDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button btNameFilter;
+        private System.Windows.Forms.TextBox tbNameSearch;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox tbTel;
+        private System.Windows.Forms.TextBox tbMemo;
+    }
+}
+
